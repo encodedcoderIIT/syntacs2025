237f6a4bb7c83d6294ffd403bfe0026c88d20046
Suresh <suresh.24csz0009@iitrpr.ac.in>
2025-03-03 15:57:49 +0530

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,32 +33,25 @@ export default function Footer() {
                 SYNTACS 2025 Team
               </Link>
             </li>
-          </ul>
-        </div>
-
-        <div className={styles.footerSection}>
-          <h3 className={styles.footerHeading}>How to Reach IIT Ropar</h3>
-          <ul className={styles.footerList}>
             <li>
-              <p>
-                <Link
-                  href="https://www.google.co.in/maps/dir/Rupnagar+Old+Bus+Stand,+Chhota+Khera,+Basant+Nagar,+Rupnagar,+Punjab/Indian+institute+of+Technology+Ropar,+Bara+Phool,+Punjab/@30.9620135,76.4693569,13z/am=t/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3905543310e70e63:0x63397d7938035537!2m2!1d76.5338308!2d30.9669692!1m5!1m1!1s0x390554d4ffffffff:0xb81f1e2708c91100!2m2!1d76.4731998!2d30.9709183?shorturl=1"
-                  target="_blank"
-                >
-                  📍 How to Reach IIT Ropar
-                </Link>
-              </p>
-              <p className="mt-2">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13684.267356494109!2d76.473305!3d30.9686169!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3905542fe45e58f7%3A0x5d16c2617cfdbdb8!2sIndian%20Institute%20Of%20Technology%E2%80%93Ropar%20(IIT%E2%80%93Ropar)!5e0!3m2!1sen!2sin!4v1697191231534!5m2!1sen!2sin"
-                  width="100%"
-                  height="200"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-              </p>
+              <Link href="/program" className={styles.navLink}>
+                Program Schedule
+              </Link>
+            </li>
+            <li>
+              <Link href="/venue" className={styles.navLink}>
+                Venue
+              </Link>
+            </li>
+            <li>
+              <Link href="/venue#how-to-reach" className={styles.navLink}>
+                How to Reach
+              </Link>
+            </li>
+            <li>
+              <Link href="/venue#nearby-places" className={styles.navLink}>
+                Near by Places
+              </Link>
             </li>
           </ul>
         </div>
@@ -66,6 +59,13 @@ export default function Footer() {
         <div className={styles.footerSection}>
           <h3 className={styles.footerHeading}>CONTACT DETAILS</h3>
           <address className={styles.contactInfo}>
+            <ul className={styles.footerList}>
+              <li>
+                <Link href="https://cse.iitrpr.ac.in/" target="_blank">
+                  CSE IIT Ropar
+                </Link>
+              </li>
+            </ul>
             <p>Indian Institute of Technology Ropar,</p>
             <p>Rupnagar, Punjab - 140001, India</p>
             <div className={styles.socialLinks}>
@@ -102,6 +102,33 @@ export default function Footer() {
               </div>
             </div>
           </address>
+        </div>
+
+        <div className={styles.footerSection}>
+          <h3 className={styles.footerHeading}>How to Reach IIT Ropar</h3>
+          <ul className={styles.footerList}>
+            <li>
+              {/* <p>
+                <Link
+                  href="https://www.google.co.in/maps/dir/Rupnagar+Old+Bus+Stand,+Chhota+Khera,+Basant+Nagar,+Rupnagar,+Punjab/Indian+institute+of+Technology+Ropar,+Bara+Phool,+Punjab/@30.9620135,76.4693569,13z/am=t/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3905543310e70e63:0x63397d7938035537!2m2!1d76.5338308!2d30.9669692!1m5!1m1!1s0x390554d4ffffffff:0xb81f1e2708c91100!2m2!1d76.4731998!2d30.9709183?shorturl=1"
+                  target="_blank"
+                >
+                  📍 How to Reach IIT Ropar
+                </Link>
+              </p> */}
+              <p className="mt-2">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13684.267356494109!2d76.473305!3d30.9686169!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3905542fe45e58f7%3A0x5d16c2617cfdbdb8!2sIndian%20Institute%20Of%20Technology%E2%80%93Ropar%20(IIT%E2%80%93Ropar)!5e0!3m2!1sen!2sin!4v1697191231534!5m2!1sen!2sin"
+                  width="100%"
+                  height="200"
+                  style={{ border: 0 }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
 
